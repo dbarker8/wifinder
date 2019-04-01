e98fda918ae9a16296ab0affcc23278ab11d4eb7
dean <dbarker8@kent.edu>
2019-04-02 01:05:12 +0200

    readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# wifinder for airbnb
+This is a browser extension that searches through all the comments on an Airbnb listing, and finds information about the wifi speed for you.
+
+Supported Browsers:
+- [Google Chrome](https://chrome.google.com/webstore/detail/wifinder-for-airbnb/ihgelolikelbdeeneamnbonfeiodacpa)
+- [Firefox](https://addons.mozilla.org/en-US/firefox/addon/wifinder-for-airbnb/)
+
+see [wifinder-server](https://github.com/dbarker8/wifinder-server) for the aws lambda server that powers this extension.
--- content.js
+++ content.js
@@ -1,3 +1,5 @@
+let keywords = ['wifi', 'internet']; // add words here
+
 var getReviewsURL = function(info){
   let { key, offset, limit, listing_id } = info;
   let url = 'https://www.airbnb.com/api/v2/reviews'
@@ -10,31 +12,21 @@ var getReviewsURL = function(info){
   + '&_limit='+limit 
   + '&_offset='+offset 
   + '&_order=language_country';
-
   return url;
 }
 
+//insert HTML and CSS with these strings because simpler than importing files in the extension
 $($.parseHTML(`<div id="wifinder-wrapper"><div id='wifinder-inside'></div></div>`)).appendTo('body');
 $('head').append('<style type="text/css">#wifinder-wrapper{z-index:9999;background-color:rgba(0,0,0,.5);position:fixed;top:0px;left:0px;right:0px;bottom:0px;display:none;}#wifinder-inside{background-color:white;height:500px;width:600px;padding:30px;border-radius:4px;margin:auto;white-space:pre-wrap;overflow:auto;margin-top: 20px;}</style>');
 $('#wifinder-wrapper').click(function(){
   $('#wifinder-wrapper').css("display", "none");
 })
 
-// get listing_id from URL and parse to int
-let page_url = window.location.href;
-let last = page_url.split('/rooms/')[1];
-let listing_id = '';
-if(last.indexOf('?')>-1) listing_id = last.split('?')[0]; // take everyting before ? mark
-if(listing_id.indexOf('/')>-1) listing_id = listing_id.splice(listing_id.indexOf('/'),1); // remove a slash if its there
-
 //select elements, determine if listing even offers wifi
 var wifiElement = $("#amenities > div > div > div > div > section > div._2h22gn > div:nth-child(1) > div:nth-child(2) > table > tbody > tr");
 var wifiText = $("#amenities > div > div > div > div > section > div._2h22gn > div:nth-child(1) > div:nth-child(2) > table > tbody > tr > td._4xosax > div");
 let wifi = false;
 if(wifiElement) if(wifiElement.text().toLowerCase()==='wifi') wifi = true;
-
-const api_url = ' https://5919f432.ngrok.io/getkey';
-let reviews = [];
 
 // supoorts up to 600 reviews. not best method but it works for this purpose
 let getReviews = async function(key, listing_id){
@@ -110,9 +102,7 @@ let getReviews = async function(key, listing_id){
     resArray.push(reviews2);
   }
 
-
   resArray.forEach(res => {
-    let keywords = ['wifi', 'internet'];
     keywords = keywords.map(item => item.toLowerCase().trim());
     keywords.forEach(keyword => {
       res.reviews.forEach(review => {
@@ -125,50 +115,77 @@ let getReviews = async function(key, listing_id){
   return reviews;
 }
 
-fetch(api_url, {
-  method: 'GET',
-})
-.then(result => result.json())
-.then(result => {
+// return key from localstorage, or get from API if expired
+let getkey = function(){
+  return new Promise(resolve => {
+    let storedKey = null;
+    let isKeyValid = false;
+    let keyres = localStorage.getItem('airbnbkey');
+    if(keyres){
+      keyres = JSON.parse(keyres);
+      // locally cache key
+      let oldestDate = new Date().setHours(new Date().getHours()-12); // guessing 12 hrs is good...
+      storedKey = keyres.key;
+      if(new Date(keyres.date) > oldestDate) isKeyValid = true;
+    }
 
-  getReviews(result.key, listing_id).then(reviews => {
-    wifiText.text(`Wifi ${"("+reviews.length+")"}`);
-
-    if(wifi && reviews.length>0){
-      wifiElement.css({
-        cursor: "pointer",
-        color: '#fd5c63',
-      })
-      wifiElement.click(function(){
-        let text = '';
-        reviews.forEach(item => {
-          text = text+'\n__________\n\n'+item.localized_date+'\n'+item.comments;
-        })
-        text = text.toLowerCase();
-        text = text.replace(/wifi/g, '<strong>wifi</strong>');
-        text = text.replace(/internet/g, '<strong>internet</strong>');
-        // show modal here
-        $('#wifinder-wrapper').css("display", "block");
-        document.getElementById('wifinder-inside').innerHTML=text;
-
-      })
+    if(isKeyValid){ return resolve(storedKey) }
+    else {
+      fetch('https://vy1fbiczn2.execute-api.us-east-1.amazonaws.com/latest/getkey')
+      .then(result => result.json())
+      .then(result => {
+        if(!result.key) return resolve(storedKey); // server error, default to something from localstorage even if expired
+        localStorage.setItem('airbnbkey', JSON.stringify({
+          date: Date.now(),
+          key: result.key
+        }));
+        resolve(result.key);
+      }).catch(err => console.log('ERR ', err))
     }
   })
+};
+
+let getReviewText = function(reviews){
+  let text = '';
+  reviews.forEach(item => {
+    text = text+'\n__________\n\n'+item.localized_date+'\n'+item.comments;
+  })
+  text = text.toLowerCase();
+  keywords.forEach(keyword => {
+    let rgx = new RegExp(keyword,"g");
+    text = text.replace(rgx, '<strong>'+keyword+'</strong>');
+  })
+  return text;
+}
+
+// get listing_id from URL and parse to int
+let page_url = window.location.href;
+let last = page_url.split('/rooms/')[1];
+let listing_id = '';
+if(last.indexOf('?')>-1) listing_id = last.split('?')[0]; // take everyting before ? mark
+if(listing_id.indexOf('/')>-1) listing_id = listing_id.splice(listing_id.indexOf('/'),1); // remove a slash if its there
 
 
-})
+// Initiate 
+let go = function(){
+  getkey().then(key => {
+    getReviews(key, listing_id).then(reviews => {
+      wifiText.text(`Wifi ${"("+reviews.length+")"}`);
+  
+      if(wifi && reviews.length>0){
+        wifiElement.css({
+          cursor: "pointer",
+          color: '#fd5c63',
+        })
+        wifiElement.click(function(){
+          let text = getReviewText(reviews);
+          // show modal here
+          $('#wifinder-wrapper').css("display", "block");
+          document.getElementById('wifinder-inside').innerHTML=text;
+        })
+      }
+    })
+  })
+}
 
-// fetch('https://www.airbnb.com')
-// .then(result => result.text())
-// .then(result => {
-//   let data = $($.parseHTML(result)).filter('meta');
-//   let key = null;
-//   // let data = $('#_bootstrap-layout-init').attr('content');
-//   // if(data){
-//   //   data = JSON.parse(data);
-//   //   key = data.api_config.key;
-//   // }
-//   console.log('aadata ', data)
-// })
-
-
+go();
